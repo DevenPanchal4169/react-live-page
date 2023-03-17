20977eec2dd58a25453839d6be677dc62195eb85
devenpanchal <devenpanchal9575@gmail.com>
2023-03-17 10:29:22 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Heloo This is react live page
         </a>
       </header>
     </div>
